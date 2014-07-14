4c1a3a322b35ee107780ffbd05bfd89dcf757dac
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-07-14 10:38:45 +0000

    Move macros to change root path to separate file
    Means we can use them elsewhere.

--- scripts/PackageLookup.cmake
+++ scripts/PackageLookup.cmake
@@ -2,6 +2,8 @@
 # or fetch it from the web and install it.
 # See https://github.com/UCL/GreatCMakeCookOff/wiki for information
 
+include(ChangeRootPath)
+
 # Sets location where external project are included
 if(NOT EXTERNAL_ROOT)
   set(EXTERNAL_ROOT "${CMAKE_BINARY_DIR}/external")
@@ -62,25 +64,6 @@ endfunction()
 
 macro(_get_sane_name name OUTVAR)
     string(REGEX REPLACE "\\-" "_" ${OUTVAR} "${name}")
-endmacro()
-
-# Changes/unchanges the root path
-# Makes it possible to look for packages stricly inside the root
-macro(_set_root_path PATH TYPE)
-    foreach(save MODE_PACKAGE MODE_INCLUDE MODE_PROGRAM MODE_LIBRARY)
-        set(_save_root_path_${save} ${CMAKE_FIND_ROOT_PATH_${save}})
-        set(CMAKE_FIND_ROOT_PATH_${save} ${TYPE})
-    endforeach()
-    set(_save_root_path "${CMAKE_FIND_ROOT_PATH}")
-    set(CMAKE_FIND_ROOT_PATH "${PATH}")
-endmacro()
-
-# Unchanges the root path
-macro(_unset_root_path)
-    foreach(save MODE_PACKAGE MODE_INCLUDE MODE_PROGRAM MODE_LIBRARY)
-        set(CMAKE_FIND_ROOT_PATH_${save} ${_save_root_path_${save}})
-    endforeach()
-    set(CMAKE_FIND_ROOT_PATH "${_save_root_path}")
 endmacro()
 
 macro(_find_package_for_lookup package REQUIRED QUIET DOWNLOAD CHECK)
