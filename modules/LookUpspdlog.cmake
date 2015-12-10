7c4f49c583b30ddd0ce4db616b89092ac4d5abe2
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-12-10 11:00:59 +0000

    Adding spdlog to find and lookup package

---
+++ modules/LookUpspdlog.cmake
@@ -0,0 +1,18 @@
+# Installs GBenchmark into build directory
+ExternalProject_Add(
+    spdlog
+    GIT_REPOSITORY https://github.com/gabime/spdlog
+    PREFIX "${EXTERNAL_ROOT}"
+    # Force separate output paths for debug and release builds to allow easy
+    # identification of correct lib in subsequent TARGET_LINK_LIBRARIES commands
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND
+      ${CMAKE_COMMAND} -E copy_directory ${EXTERNAL_ROOT}/src/spdlog/include ${EXTERNAL_ROOT}/include
+
+    # Wrap download, configure and build steps in a script to log output
+    UPDATE_COMMAND ""
+    LOG_DOWNLOAD ON LOG_CONFIGURE ON LOG_BUILD ON LOG_INSTALL ON
+)
+
+add_recursive_cmake_step(spdlog DEPENDEES install)
