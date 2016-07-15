654aa59b9bd95fae88b3ebb2aacbd5b0ed14663f
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2016-07-15 17:57:30 +0100

    Almost there! :scream_cat: All verbose

--- modules/FindFFTW3.cmake
+++ modules/FindFFTW3.cmake
@@ -24,25 +24,39 @@ if(FFTW3_INCLUDE_DIR AND FFTW3_LIBRARIES)
 endif()
 
 macro(find_specific_libraries KIND PARALLEL)
+  message(STATUS "Checking for ${KIND} and ${PARALLEL}")
+  message(STATUS "Looking for: fftw3${SUFFIX_${KIND}}${SUFFIX_${PARALLEL}}${SUFFIX_FINAL} ")
   find_library(FFTW3_${KIND}_${PARALLEL}_LIBRARY NAMES
     fftw3${SUFFIX_${KIND}}${SUFFIX_${PARALLEL}}${SUFFIX_FINAL} HINTS ${HINT_DIRS})
+  message(STATUS "Library: ${FFTW3_${KIND}_${PARALLEL}_LIBRARY} ?")
   if(FFTW3_${KIND}_${PARALLEL}_LIBRARY MATCHES fftw3)
+    message(STATUS "Library: ${FFTW3_${KIND}_${PARALLEL}_LIBRARY} matches fftw3")
     list(APPEND FFTW3_LIBRARIES ${FFTW3_${KIND}_${PARALLEL}_LIBRARY})
     set(FFTW3_${KIND}_${PARALLEL}_FOUND TRUE)
+
+
     STRING(TOLOWER "${KIND}" kind)
     STRING(TOLOWER "${PARALLEL}" parallel)
+    message(STATUS "Setting target: ${kind} /// ${parallel}")
     if(FFTW3_${kind}_${parallel}_LIBRARY MATCHES "\\.a$")
       add_library(fftw3::${kind}::${parallel} STATIC IMPORTED GLOBAL)
     else()
       add_library(fftw3::${kind}::${parallel} SHARED IMPORTED GLOBAL)
     endif()
+
+    # MPI Has a different included library than the others
+    # FFTW3_INCLUDE_DIR_PARALLEL will change depending of which on is used.
     set(FFTW3_INCLUDE_DIR_PARALLEL ${FFTW3_INCLUDE_DIR} )
     if(PARALLEL STREQUAL "MPI")
       set(FFTW3_INCLUDE_DIR_PARALLEL ${FFTW3_${PARALLEL}_INCLUDE_DIR})
     endif()
+
     set_target_properties(fftw3::${kind}::${parallel} PROPERTIES
       IMPORTED_LOCATION "${FFTW3_${KIND}_${PARALLEL}_LIBRARY}"
       INTERFACE_INCLUDE_DIRECTORIES "${FFTW3_INCLUDE_DIR_PARALLEL}")
+
+    # adding target properties to the different cases
+    ##   MPI
     if(PARALLEL STREQUAL "MPI")
       if(NOT MPI_C_FOUND)
         message(FATAL_ERROR "Please, find mpi libraries before FFTW")
@@ -56,6 +70,22 @@ macro(find_specific_libraries KIND PARALLEL)
         if(MPI_C_FLAGS)
           target_compile_options(fftw3::${kind}::mpi INTERFACE ${MPI_C_FLAGS})
         endif()
+      endif()
+    endif()
+    ##   OpenMP
+    if(PARALLEL STREQUAL "OPENMP")
+      if(NOT OPENMP_FOUND)
+        message(FATAL_ERROR "Please, find openmp libraries before FFTW")
+      else()
+        if(OPENMP_C_FLAGS)
+          target_compile_options(fftw3::${kind}::openmp INTERFACE ${OPENMP_C_FLAGS})
+        endif()
+      endif()
+    endif()
+    ##  THREADED
+    if(PARALLEL STREQUAL "THREADED")
+      if(CMAKE_THREAD_LIBS_INIT)
+        target_link_libraries(fftw3::${kind}::thread ${CMAKE_THREAD_LIBS_INIT})
       endif()
     endif()
 
@@ -83,6 +113,14 @@ list(FIND FFTW3_FIND_COMPONENTS OPENMP LOOK_FOR_OPENMP)
 list(FIND FFTW3_FIND_COMPONENTS MPI LOOK_FOR_MPI)
 list(FIND FFTW3_FIND_COMPONENTS SERIAL LOOK_FOR_SERIAL)
 
+# set serial as default if none parallel component has been set
+if((LOOK_FOR_THREADED LESS 0) AND (LOOK_FOR_MPI LESS 0) AND
+    (LOOK_FOR_OPENMP LESS 0))
+  set(LOOK_FOR_SERIAL 1)
+endif()
+
+
+
 if(WIN32)
   set(HINT_DIRS ${FFTW3_DIRECTORY} $ENV{FFTW3_DIRECTORY})
 else()
@@ -101,7 +139,9 @@ if (LOOK_FOR_MPI)  # Probably is going to be the same as fftw3.h
 endif()
 
 function(find_version OUTVAR LIBRARY SUFFIX)
+    message(STATUS "Finding version")
     file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/fftw${SUFFIX}/main.c
+      # TODO: do we need to add include for mpi headers?
       "#include <fftw3.h>
        #include <stdio.h>
        int main(int nargs, char const *argv[]) {
@@ -109,7 +149,8 @@ function(find_version OUTVAR LIBRARY SUFFIX)
            return 0;
        }"
   )
-  if(NOT CMAKE_CROSSCOMPILING)
+if(NOT CMAKE_CROSSCOMPILING)
+  message(STATUS "${LIBRARY} ${FFTW3_INCLUDE_DIR}")
     try_run(RUN_RESULT COMPILE_RESULT
         "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/fftw${SUFFIX}/"
         "${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/fftw${SUFFIX}/main.c"
@@ -149,11 +190,14 @@ unset(FFTW3_LIBRARIES)
 set(FFTW3_INCLUDE_DIRS ${FFTW3_INCLUDE_DIR} ) # TODO what's for?
 
 foreach(KIND SINGLE DOUBLE LONGDOUBLE)
+  message(STATUS "Checking ${KIND}")
   if(LOOK_FOR_${KIND} LESS 0)
     continue()
   endif()
   foreach(PARALLEL SERIAL MPI OPENMP THREADED)
+    message(STATUS "Checking for ${PARALLEL}")
     if(LOOK_FOR_${PARALLEL} LESS 0)
+      message(STATUS "skipping macro ${PARALLEL}:${LOOK_FOR_${PARALLEL}}")
       continue()
     endif()
     find_specific_libraries(${KIND} ${PARALLEL})
@@ -161,79 +205,34 @@ foreach(KIND SINGLE DOUBLE LONGDOUBLE)
 endforeach()
 
 
-
-
-
-if(FFTW3_SINGLE_FOUND AND FFTW3_INCLUDE_DIR)
-    find_version(FFTW3_VERSION_STRING ${FFTW3_SINGLE_LIBRARY} f)
-elseif(FFTW3_DOUBLE_FOUND AND FFTW3_INCLUDE_DIR)
-    find_version(FFTW3_VERSION_STRING ${FFTW3_DOUBLE_LIBRARY} "")
-elseif(FFTW3_LONGDOUBLE_FOUND AND FFTW3_INCLUDE_DIR)
-    find_version(FFTW3_VERSION_STRING ${FFTW3_LONGDOUBLE_LIBRARY} "l")
-elseif(FFTW3_THREADED_FOUND AND FFTW3_INCLUDE_DIR)  # TODO What happens when you ask for double and threaded?
-    find_version(FFTW3_VERSION_STRING ${FFTW3_THREADED_LIBRARY} "t") # TODO is this ok?
-elseif(FFTW3_OPENMP_FOUND AND FFTW3_INCLUDE_DIR)  # TODO What happens when you ask for double and openmp?
-    find_version(FFTW3_VERSION_STRING ${FFTW3_OPENMP_LIBRARY} "t") # TODO is this ok?
+message(STATUS "my library is:  ${FFTW3_LIBRARIES}")
+message(STATUS ($<bool:${FFTW3_FOUND}>))
+message(STATUS "${FFTW3_INCLUDE_DIR}")
+if(FFTW3_INCLUDE_DIR)
+  # TODO: This just look on the simple case, but that may not been defined...
+  # How can I get the last one?
+  set(KIND "SINGLE")
+  set(PARALLEL "SERIAL")
+  message(STATUS "${FFTW3_${KIND}_${PARALLEL}_LIBRARY}")
+  message(STATUS "${SUFFIX_${KIND}}${SUFFIX_${PARALLEL}}${SUFFIX_FINAL}")
+  find_version(FFTW3_VERSION_STRING ${FFTW3_${KIND}_${PARALLEL}_LIBRARY}
+    ${SUFFIX_${KIND}}${SUFFIX_${PARALLEL}}${SUFFIX_FINAL})
 endif()
+message(STATUS "my version is: ${FFTW3_VERSION_STRING}")
 
+
+message(STATUS "Printing all the variables:")
+message(STATUS  "ReqVars: FFT_LIB = ${FFTW3_LIBRARIES} FFT_INC = ${FFTW3_INCLUDE_DIR}")
+message(STATUS "version=${FFTW3_VERSION_STRING}")
+
+# FIXME: fails if use REQUIRED.
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set FFTW3_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(FFTW3
     REQUIRED_VARS FFTW3_LIBRARIES FFTW3_INCLUDE_DIR
     VERSION_VAR FFTW3_VERSION_STRING
-    HANDLE_COMPONENTS
+    #HANDLE_COMPONENTS
+    # FIXME: HANDLE_COMPONENTS fails in this current implementation because
+    # COMPONENTS inputs are as SINGLE, but SINGLE_SERIAL is found.
 )
-if(FFTW3_FOUND)
-  if(FFTW3_SINGLE_LIBRARY MATCHES fftw3f)
-    if(FFTW3_SINGLE_LIBRARY MATCHES "\\.a$")
-
-    else()
-      add_library(fftw3::single SHARED IMPORTED GLOBAL)
-    endif()
-    set_target_properties(fftw3::double PROPERTIES
-      IMPORTED_LOCATION "${FFTW3_SINGLE_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${FFTW3_INCLUDE_DIR}")
-  endif()
-  if(FFTW3_DOUBLE_LIBRARY MATCHES fftw3)
-    if(FFTW3_DOUBLE_LIBRARY MATCHES "\\.a$")
-      add_library(fftw3::double STATIC IMPORTED GLOBAL)
-    else()
-      add_library(fftw3::double SHARED IMPORTED GLOBAL)
-    endif()
-    set_target_properties(fftw3::double PROPERTIES
-      IMPORTED_LOCATION "${FFTW3_DOUBLE_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${FFTW3_INCLUDE_DIR}")
-  endif()
-  if(FFTW3_LONGDOUBLE_LIBRARY MATCHES fftw3l)
-    if(FFTW3_LONGDOUBLE_LIBRARY MATCHES "\\.a$")
-      add_library(fftw3::longdouble STATIC IMPORTED GLOBAL)
-    else()
-      add_library(fftw3::longdouble SHARED IMPORTED GLOBAL)
-    endif()
-    set_target_properties(fftw3::longdouble PROPERTIES
-      IMPORTED_LOCATION "${FFTW3_LONGDOUBLE_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${FFTW3_INCLUDE_DIR}")
-  endif()
-  # TODO do I need this?
-  if(FFTW3_THREADED_LIBRARY MATCHES fftw3_threads)
-    if(FFTW3_THREADED_LIBRARY MATCHES "\\.a$")
-      add_library(fftw3::threaded STATIC IMPORTED GLOBAL)
-    else()
-      add_library(fftw3::threaded SHARED IMPORTED GLOBAL)
-    endif()
-    set_target_properties(fftw3::threaded PROPERTIES
-      IMPORTED_LOCATION "${FFTW3_THREADED_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${FFTW3_INCLUDE_DIR}")
-  endif()
-  if(FFTW3_OPENMP_LIBRARY MATCHES fftw3_omp)
-    if(FFTW3_OPENMP_LIBRARY MATCHES "\\.a$")
-      add_library(fftw3::openmp STATIC IMPORTED GLOBAL)
-    else()
-      add_library(fftw3::openmp SHARED IMPORTED GLOBAL)
-    endif()
-    set_target_properties(fftw3::openmp PROPERTIES
-      IMPORTED_LOCATION "${FFTW3_OPENMP_LIBRARY}"
-      INTERFACE_INCLUDE_DIRECTORIES "${FFTW3_INCLUDE_DIR}")
-  endif()
-endif()
