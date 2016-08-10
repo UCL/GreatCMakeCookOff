0073f3a684ed03fc445e629b955ff251ce41c10b
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2016-08-10 10:05:29 +0100

    Add new psudorelease of gbenchmark from ucl spac

--- hunter_recipes/projects/GBenchmark/hunter.cmake
+++ hunter_recipes/projects/GBenchmark/hunter.cmake
@@ -25,6 +25,17 @@ hunter_add_version(
     4f778985dce02d2e63262e6f388a24b595254a93
 )
 
+hunter_add_version(
+    PACKAGE_NAME
+    GBenchmark
+    VERSION
+    "1.1.0"
+    URL
+    "https://github.com/mdavezac/benchmark/archive/v1.1.0.tar.gz"
+    SHA1
+    772372564531dc0023037367a122391326bd1dc0
+)
+
 hunter_cmake_args(GBenchmark CMAKE_ARGS BENCHMARK_ENABLE_TESTING=OFF)
 hunter_configuration_types(GBenchmark CONFIGURATION_TYPES Release)
 # Pick a download scheme
