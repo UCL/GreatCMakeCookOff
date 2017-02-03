63796fc17b50fc573482a13d04165d9c5512da22
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2017-02-03 16:41:48 +0000

    Incorrect capitalization. Thanks macos.

--- modules/FindCatch.cmake
+++ modules/FindCatch.cmake
@@ -19,17 +19,17 @@ endif()
 find_path(CATCH_INCLUDE_DIR catch.hpp PATHS ${EXTERNAL_ROOT}/include)
 if(CATCH_INCLUDE_DIR)
   file(
-    STRINGS ${CATCH_INCLUDE_DIR}/Catch.hpp
+    STRINGS ${CATCH_INCLUDE_DIR}/catch.hpp
     CATCH_VERSION_STRING
     REGEX "[ ]+Catch[ ]+v([0-9]*\\.[0-9]*\\.[0-9]*)"
-    )
+  )
   string(
     REGEX REPLACE
     ".*[ ]+Catch[ ]+v([0-9]*\\.[0-9]*\\.[0-9]*)"
     "\\1"
     CATCH_VERSION_STRING
     "${CATCH_VERSION_STRING}"
-    )
+  )
 endif()
 
 set(CATCH_INCLUDE_DIRS ${CATCH_INCLUDE_DIR} )
