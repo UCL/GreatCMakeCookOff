0d5464109b9aacc0589f0df741f7bec3a2f77bf4
David Pérez-Suárez <dps.helio@gmail.com>
2018-06-19 17:01:25 +0100

    Adds option to select a particular tag for Google's benchmark

--- modules/LookUpGBenchmark.cmake
+++ modules/LookUpGBenchmark.cmake
@@ -1,13 +1,23 @@
 # Installs GBenchmark into build directory
 if(GBenchmark_ARGUMENTS)
-  cmake_parse_arguments(GBenchmark "" "GIT_REPOSITORY" "" ${GBenchmark_ARGUMENTS})
+    cmake_parse_arguments(GBenchmark
+        ""
+        "GIT_REPOSITORY;GIT_TAG"
+        ""
+        ${GBenchmark_ARGUMENTS}
+    )
 endif()
+
 if(NOT GBenchmark_GIT_REPOSITORY)
   set(GBenchmark_GIT_REPOSITORY  https://github.com/google/benchmark.git)
 endif()
 
+if(NOT GBenchmark_GIT_TAG)
+  set(GBenchmark_GIT_TAG master)
+endif()
+
 include(PassonVariables)
-passon_variables(GTest
+passon_variables(GBenchmark
     FILENAME "${EXTERNAL_ROOT}/src/GBenchmarkVariables.cmake"
     PATTERNS
         "CMAKE_[^_]*_R?PATH"
@@ -20,6 +30,7 @@ passon_variables(GTest
 ExternalProject_Add(
     Lookup-GBenchmark
     GIT_REPOSITORY ${GBenchmark_GIT_REPOSITORY}
+    GIT_TAG ${GBenchmark_GIT_TAG}
     PREFIX "${EXTERNAL_ROOT}"
     # Force separate output paths for debug and release builds to allow easy
     # identification of correct lib in subsequent TARGET_LINK_LIBRARIES commands
@@ -30,7 +41,10 @@ ExternalProject_Add(
 
     # Wrap download, configure and build steps in a script to log output
     UPDATE_COMMAND ""
-    LOG_DOWNLOAD ON LOG_CONFIGURE ON LOG_BUILD ON LOG_INSTALL ON
+    LOG_DOWNLOAD ON
+    LOG_CONFIGURE ON
+    LOG_BUILD ON
+    LOG_INSTALL ON
 )
 
 add_recursive_cmake_step(Lookup-GBenchmark DEPENDEES install)
